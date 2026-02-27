d77bc20b8c47dd244bd4a91505f5baacff876748
Hyun Seung Cho <josh100as@gmail.com>
2026-02-27 13:31:47 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long int n;
+    cin >> n;
+    cout << n * 4;
+    
+    return 0;
+}
